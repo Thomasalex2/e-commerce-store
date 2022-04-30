8464e197012b8330396c8ed57c97de6b92db519d
Thomas Alex <thomas.a@tiflabs.in>
2022-04-30 15:22:34 +0530

    - Added Navlinks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, NavLink } from "react-router-dom";
+import { ActiveLinkStyle } from "./helpers/helpers"
 
 function App() {
+
   return (
     <div>
     <nav class="nav-bar">
         <div class="nav-elements-left">
           <button class="nav-btn-style"><span class="material-icons">menu</span></button>
-          <Link to="/homepage"><h4>The Pencil Store</h4></Link>
+          <NavLink style={ActiveLinkStyle} to="/homepage"><h4>The Pencil Store</h4></NavLink>
         </div>
         <div class="nav-elements-right">
-          <Link to="/products"><h5>All Products</h5></Link>
+          <NavLink style={ActiveLinkStyle} to="/products"><h5>All Products</h5></NavLink>
             <div class="nav-search-container">
                 <button class="nav-btn-style"><span class="material-icons">search</span></button>
                 <input type="text" class="nav-search" placeholder="Type to search"/>
             </div>
             <button class="nav-btn-style"><span class="material-icons">account_circle</span></button>
-            <Link to="/cart" class="nav-btn-style"><span class="material-icons">shopping_cart</span></Link>
-            <Link to="/wishlist" class="nav-btn-style"><span class="material-icons">star_rate</span></Link>
-            <Link to="/login" class="nav-btn-style">Login</Link>
+            <NavLink style={ActiveLinkStyle} to="/wishlist" class="nav-btn-style"><span class="material-icons">star_rate</span></NavLink>
+            <NavLink style={ActiveLinkStyle} to="/cart" class="nav-btn-style"><span class="material-icons">shopping_cart</span></NavLink>
+            <NavLink style={ActiveLinkStyle} to="/login" class="nav-btn-style">Login</NavLink>
             
         </div>
     </nav>
